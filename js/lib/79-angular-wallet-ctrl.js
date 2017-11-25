b683a8f52ef6b0832ff8028ca5b398c2c1269e88
jaros1 <yje6P723Pf5pnsgpsY6ZLeJ4J3oC0I>
2017-11-25 14:25:26 +0100

    https://github.com/jaros1/Money-Network-W2/issues/38 Added 13 screen dumps with W2 standalone usage and W2 used from MoneyNetwork

--- js/lib/79-angular-wallet-ctrl.js
+++ js/lib/79-angular-wallet-ctrl.js
@@ -284,6 +284,115 @@ angular.module('MoneyNetworkW2')
             }) ;
         }; // send_money
 
+        // get list of screendumps in screendumps folder
+        self.screendumps = [] ;
+        (function () {
+            MoneyNetworkAPILib.z_file_get(controller, {inner_path: 'screendumps/content.json'}, function (content_str) {
+                var pgm = controller + ' start z_file_get callback 1: ';
+                var content, files, filename, text ;
+                if (!content_str) {
+                    console.log(controller + ': error. cannot show screendumps. screendumps/content.json file was not found') ;
+                    return ;
+                }
+                try {
+                    content = JSON.parse(content_str) ;
+                }
+                catch (e) {
+                    console.log(controller + ': error. cannot show screendumps. screendumps/content.json is invalid. error = ' + e.message) ;
+                    return ;
+                }
+                if (!content.files_optional || !Object.keys(content.files_optional).length) {
+                    console.log(controller + ': error. cannot show screendumps. no screendumps were found in screendumps/content.json') ;
+                    return ;
+                }
+                for (filename in content.files_optional) {
+                    try {
+                        text = filename.substr(0,filename.length-4).split('-') ;
+                        text.splice(0, 2) ;
+                        text.splice(1, 0, ':') ;
+                        text = text.join(' ') ;
+                        text = text.charAt(0).toUpperCase() + text.slice(1) + ':';
+                    }
+                    catch (e) {
+                        text = filename ;
+                    }
+                    self.screendumps.push({
+                        filename: filename,
+                        text: text,
+                        src: 'screendumps/' + filename,
+                        show: false}) ;
+                }
+                self.screendumps.sort(function (a,b) {
+                    var fileid1, fileid2 ;
+                    fileid1 = parseInt(a.filename.split('-')[0]) ;
+                    fileid2 = parseInt(b.filename.split('-')[0]) ;
+                    return fileid1 - fileid2 ;
+                }) ;
+
+                console.log(controller + ': self.screendumps = ' + JSON.stringify(self.screendumps)) ;
+
+                // check files in screendumps folder
+                ZeroFrame.cmd("fileList", ['screendumps'], function(files) {
+                    var pgm = controller + ' start fileList callback 3: ';
+                    var i, j ;
+                    console.log(pgm + 'files = ' + JSON.stringify(files)) ;
+                    //files = ["2020-w2-w2-create-wallet.png", "1400-mn-wallet-test-start.png", "1380-mn-chat-send-money.png", "1300-mn-chat.png", "1401-mn-wallet-test-start.png", "2010-w2-w2-cert-and-merger-permission.png", "1000-mn-login-cert-and-merger-notifications.png", "1381-mn-chat-send-money.png", "1450-mn-wallet-test-ok.png", "content.json", "1010-mn-login-register-new-user.png", "1100-mn-account-update-user-info.png"];
+                    for (i=0 ; i<files.length ; i++) {
+                        filename = files[i] ;
+                        for (j=0 ; j<self.screendumps.length ; j++) {
+                            if (self.screendumps[j].filename == files[i]) {
+                                self.screendumps[j].files = true ;
+                                break ;
+                            }
+                        } // for j
+                    } // for i
+                    console.log(controller + ': self.screendumps = ' + JSON.stringify(self.screendumps)) ;
+
+                }) ; // fileList callback 2
+
+            }) ; // z_file_get callback 1
+        })() ;
+
+        // show/hide screendumps.
+        self.show_hide = function (prefix) {
+            var pgm = controller + '.show_hide: ' ;
+            var re, i, filename, download, get_screendump ;
+            re = new RegExp(prefix) ;
+            console.log(pgm + 're = ', re) ;
+            download = [] ;
+            for (i=0 ; i<self.screendumps.length ; i++) {
+                filename = self.screendumps[i].filename ;
+                if (filename.match(re)) {
+                    self.screendumps[i].show = !self.screendumps[i].show ;
+                    if (self.screendumps[i].show && !self.screendumps[i].files) download.push(self.screendumps[i].filename) ;
+                }
+            }
+            console.log(pgm + 'self.screendumps = ' + JSON.stringify(self.screendumps)) ;
+            console.log(pgm + 'downloads = ' + JSON.stringify(download)) ;
+
+            get_screendump = function() {
+                var pgm = controller + '.show_hide.get_screendump: ' ;
+                var filename, inner_path ;
+                if (!download.length) return ;
+                filename = download.shift() ;
+                inner_path = 'screendumps/' + filename ;
+                console.log(pgm + 'downloading ' + inner_path) ;
+                ZeroFrame.cmd("fileGet", {inner_path: inner_path, required: true, format: 'base64'}, function(data) {
+                    var i ;
+                    if (data) {
+                        for (i=0 ; i<self.screendumps.length ; i++) if (self.screendumps[i].filename == filename) self.screendumps[i].files = true ;
+                        console.log(pgm + filename + ' download OK') ;
+                        $rootScope.$apply() ;
+                    }
+                    else console.log(pgm + filename + ' download failed') ;
+                    // next download
+                    get_screendump() ;
+                }) ;
+            } ; // get_screendump
+            get_screendump() ;
+
+        } ; // show_hide
+
         // end WalletCtrl
     }])
 
